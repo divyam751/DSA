a1e870cba4dc1e873320cefd4ed23b337fd9c555
Divyam Chauhan <mi.divyam@gmail.com>
2023-07-26 10:46:06 +0530

    Bubble Sort

---
+++ BubbleSort.js
@@ -0,0 +1,14 @@
+// Bubble Sort
+
+let arr = [5, 4, 0, 1, 2];
+
+for (let i = 0; i < arr.length - 1; i++) {
+  for (let j = 0; j < arr.length - 1; j++) {
+    if (arr[j] > arr[j + 1]) {
+      let temp = arr[j];
+      arr[j] = arr[j + 1];
+      arr[j + 1] = temp;
+    }
+  }
+}
+console.log(arr);
